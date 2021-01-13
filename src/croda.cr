f55d7d03c053381793ec52713fb6a672c0f21e09
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2021-01-12 23:39:25 -0500

    Move plugin macros into the base plugin
    First of many minor differences between Croda and Roda is that in order to "bootstrap" the plugin system, I have to include the base plugin instance methods instead of extending the class methods because you can't call a macro from a module you extended (Maybe you can in certain situations???).

--- src/croda.cr
+++ src/croda.cr
@@ -3,50 +3,6 @@ require "./croda/*"
 
 abstract class Croda
   include HTTP::Handler
-
-  macro croda_plugin(instance_methods_class, class_methods_class)
-    {% if imc = instance_methods_class.resolve? %}
-      include {{ imc }}
-    {% end %}
-
-    {% if cmc = class_methods_class.resolve? %}
-      extend {{ cmc }}
-    {% end %}
-  end
-
-  macro request_plugin(instance_methods_class, class_methods_class)
-    {% if imc = instance_methods_class.resolve? %}
-      class ::Croda::CrodaRequest
-        include {{ imc }}
-      end
-    {% end %}
-
-    {% if cmc = class_methods_class.resolve? %}
-      class ::Croda::CrodaRequest
-        extend {{ cmc }}
-      end
-    {% end %}
-  end
-
-  macro response_plugin(instance_methods_class, class_methods_class)
-    {% if imc = instance_methods_class.resolve? %}
-      class ::Croda::CrodaResponse
-        include {{ imc }}
-      end
-    {% end %}
-
-    {% if cmc = class_methods_class.resolve? %}
-      class ::Croda::CrodaResponse
-        extend {{ cmc }}
-      end
-    {% end %}
-  end
-
-  macro plugin(type)
-    croda_plugin({{ "#{type}::InstanceMethods".id }}, {{ "#{type}::ClassMethods".id }})
-    request_plugin({{ "#{type}::RequestMethods".id }}, {{ "#{type}::RequestClassMethods".id }})
-    response_plugin({{ "#{type}::ResponseMethods".id }}, {{ "#{type}::ResponseClassMethods".id }})
-  end
-
+  include CrodaPlugins::Base::InstanceMethods
   plugin CrodaPlugins::Base
 end
