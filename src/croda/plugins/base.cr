77f1822b76bfd85d23c482cf2cde99d1c120bcb6
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2021-01-17 16:46:23 -0500

    Add the most basic of request handling

--- src/croda/plugins/base.cr
+++ src/croda/plugins/base.cr
@@ -65,16 +65,53 @@ class Croda
         end
 
         def execute(context)
-          request = Croda::CrodaRequest.new(context.request)
-          with self yield request
+          request = Croda::CrodaRequest.new(context.request, self)
+          catch :halt do
+            with self yield request
+          end
         end
       end
 
       module RequestMethods
-        def initialize(@request : HTTP::Request)
+        def initialize(@request : HTTP::Request, @app : ::Croda)
         end
 
-        forward_missing_to @request
+        def get
+          if get?
+            with @app yield
+            throw :halt
+          end
+        end
+
+        def get(path)
+          if get? && path_matches?(path)
+            with @app yield
+            throw :halt
+          end
+        end
+
+        def post
+          if post?
+            with @app yield
+            throw :halt
+          end
+        end
+
+        private def get?
+          request_method == "GET"
+        end
+
+        private def post?
+          request_method == "POST"
+        end
+
+        private def path_matches?(path)
+          @request.path == path
+        end
+
+        private def request_method
+          @request.method.upcase
+        end
       end
     end
   end
