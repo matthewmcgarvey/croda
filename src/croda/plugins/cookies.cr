0a2512345ea6d33987c961f40cf206ebaee39623
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2025-12-27 14:27:56 -0600

    Fix up cookies plugin

--- src/croda/plugins/cookies.cr
+++ src/croda/plugins/cookies.cr
@@ -1,6 +1,14 @@
 abstract class Croda
   module CrodaPlugins
     module Cookies
+      protected class_getter domain : String?
+      protected class_getter path : String?
+
+      def self.configure(_app : Croda.class, domain : String? = nil, path : String? = nil)
+        @@domain = domain
+        @@path = path
+      end
+
       module RequestMethods
         def cookies : HTTP::Cookies
           HTTP::Cookies.from_client_headers(headers)
@@ -8,7 +16,7 @@ abstract class Croda
       end
 
       module ResponseMethods
-        def delete_cookie(key, **values)
+        def delete_cookie(key : String, **values) : Nil
           set_cookie(key, {
             value:   "",
             path:    nil,
@@ -18,19 +26,21 @@ abstract class Croda
           }.merge(values))
         end
 
-        def set_cookie(key, value : String)
-          set_cookie(key, {value: value})
-        end
-
-        private def set_cookie(key, values : NamedTuple)
+        def set_cookie(key : String, value : String, **attrs) : Nil
+          cookie_attrs = default_cookie_opts.merge(attrs)
           cookie = HTTP::Cookie.new(
-            **values,
-            name: key.to_s,
+            **cookie_attrs,
+            name: key,
+            value: value,
             http_only: true
           )
           cookies = HTTP::Cookies.from_server_headers(headers)
           cookies << cookie
           cookies.add_response_headers(headers)
+        end
+
+        private def default_cookie_opts : NamedTuple(domain: String?, path: String?)
+          {domain: Cookies.domain, path: Cookies.path}
         end
       end
     end
